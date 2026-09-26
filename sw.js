27bcea2a5ef13597f2c116d0615d0f887d1242b8
Tdeeel <108835941+Tdeeel@users.noreply.github.com>
2026-09-26 13:38:10 +0800

    Refresh offline web cache for caregiver display

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'day-night-clock-v1';
+const CACHE = 'day-night-clock-v2';
 const FILES = ['./','index.html','manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
